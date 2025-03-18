0a8723775cc9e29cbeac9f01a9da20138c939e13
Ian Rokas <grokas@splunk.com>
2025-03-18 05:23:12 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- myip_connector.py
+++ myip_connector.py
@@ -1,6 +1,6 @@
 # File: myip_connector.py
 #
-# Copyright (c) 2018-2023 Splunk Inc.
+# Copyright (c) 2018-2025 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,12 +18,14 @@
 import json
 
 import phantom.app as phantom
+
 # Usage of the consts file is recommended
 # from myip_consts import *
 import requests
 from bs4 import BeautifulSoup
 from phantom.action_result import ActionResult
 from phantom.base_connector import BaseConnector
+
 
 try:
     import md5
@@ -41,11 +43,9 @@ class RetVal(tuple):
 
 
 class MyipConnector(BaseConnector):
-
     def __init__(self):
-
         # Call the BaseConnectors init first
-        super(MyipConnector, self).__init__()
+        super().__init__()
 
         self._state = None
 
@@ -56,74 +56,68 @@ class MyipConnector(BaseConnector):
         self._python_version = None
 
     def _process_empty_reponse(self, response, action_result):
-
         if response.status_code == 200:
             return RetVal(phantom.APP_SUCCESS, {})
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, "Empty response and no information in the header"), None)
 
     def _process_html_response(self, response, action_result):
-
         # An html response, treat it like an error
         status_code = response.status_code
 
         try:
             soup = BeautifulSoup(response.text, "html.parser")
             error_text = soup.text
-            split_lines = error_text.split('\n')
+            split_lines = error_text.split("\n")
             split_lines = [x.strip() for x in split_lines if x.strip()]
-            error_text = '\n'.join(split_lines)
+            error_text = "\n".join(split_lines)
         except:
             error_text = "Cannot parse error details"
 
-        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code,
-                error_text)
+        message = f"Status Code: {status_code}. Data from server:\n{error_text}\n"
 
-        message = message.replace('{', '{{').replace('}', '}}')
+        message = message.replace("{", "{{").replace("}", "}}")
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
     def _process_json_response(self, r, action_result):
-
         # Try a json parse
         try:
             resp_json = r.json()
         except Exception as e:
-            return RetVal(action_result.set_status(phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))), None)
+            return RetVal(action_result.set_status(phantom.APP_ERROR, f"Unable to parse JSON response. Error: {e!s}"), None)
 
         # Please specify the status codes here
         if 200 <= r.status_code < 399:
             return RetVal(phantom.APP_SUCCESS, resp_json)
 
         # You should process the error returned in the json
-        message = "Error from server. Status Code: {0} Data from server: {1}".format(
-                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
+        message = "Error from server. Status Code: {} Data from server: {}".format(r.status_code, r.text.replace("{", "{{").replace("}", "}}"))
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
     def _process_response(self, r, action_result):
-
         # store the r_text in debug data, it will get dumped in the logs if the action fails
-        if hasattr(action_result, 'add_debug_data'):
-            action_result.add_debug_data({'r_status_code': r.status_code})
-            action_result.add_debug_data({'r_text': r.text})
-            action_result.add_debug_data({'r_headers': r.headers})
+        if hasattr(action_result, "add_debug_data"):
+            action_result.add_debug_data({"r_status_code": r.status_code})
+            action_result.add_debug_data({"r_text": r.text})
+            action_result.add_debug_data({"r_headers": r.headers})
 
         # Process each 'Content-Type' of response separately
 
         # Process a json response
-        content_type = r.headers.get('Content-Type', '')
+        content_type = r.headers.get("Content-Type", "")
 
         # this service returns the JSON with the content type set
         # as text !!, so need to handle that
-        if 'json' in content_type or 'text' in content_type:
+        if "json" in content_type or "text" in content_type:
             return self._process_json_response(r, action_result)
 
         # Process an HTML resonse, Do this no matter what the api talks.
         # There is a high chance of a PROXY in between phantom and the rest of
         # world, in case of errors, PROXY's return HTML, this function parses
         # the error and adds it to the action_result.
-        if 'html' in content_type:
+        if "html" in content_type:
             return self._process_html_response(r, action_result)
 
         # it's not content-type that is to be parsed, handle an empty response
@@ -131,13 +125,14 @@ class MyipConnector(BaseConnector):
             return self._process_empty_reponse(r, action_result)
 
         # everything else is actually an error at this point
-        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
-                r.status_code, r.text.replace('{', '{{').replace('}', '}}'))
+        message = "Can't process response from server. Status Code: {} Data from server: {}".format(
+            r.status_code, r.text.replace("{", "{{").replace("}", "}}")
+        )
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
     def _is_ip(self, input_ip_address):
-        """ Function that checks given address and return True if address is valid IPv4 or IPV6 address.
+        """Function that checks given address and return True if address is valid IPv4 or IPV6 address.
         :param input_ip_address: IP address
         :return: status (success/failure)
         """
@@ -155,41 +150,39 @@ class MyipConnector(BaseConnector):
         return True
 
     def _make_rest_call(self, query_object, action_result):
-
         config = self.get_config()
 
         resp_json = None
 
-        time_stamp = datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S')
+        time_stamp = datetime.utcnow().strftime("%Y-%m-%d_%H:%M:%S")
 
         # Create a URL, this will be used to create the signature
-        url_part_1 = '{0}/{1}/api_id/{2}/api_key/{3}'.format(self._base_url, query_object, config['api_id'], config['api_key'])
-        url_part_2 = 'timestamp/{0}'.format(time_stamp)
-        url_for_signature = '{0}/{1}'.format(url_part_1, url_part_2)
+        url_part_1 = "{}/{}/api_id/{}/api_key/{}".format(self._base_url, query_object, config["api_id"], config["api_key"])
+        url_part_2 = f"timestamp/{time_stamp}"
+        url_for_signature = f"{url_part_1}/{url_part_2}"
 
         signature = md5.new(url_for_signature).hexdigest() if self._python_version < 3 else hashlib.md5(url_for_signature.encode()).hexdigest()
 
-        url = '{0}/signature/{1}/{2}'.format(url_part_1, signature, url_part_2)
+        url = f"{url_part_1}/signature/{signature}/{url_part_2}"
 
         try:
             r = requests.get(url)
         except Exception as e:
-            return RetVal(action_result.set_status( phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(str(e))), resp_json)
+            return RetVal(action_result.set_status(phantom.APP_ERROR, f"Error Connecting to server. Details: {e!s}"), resp_json)
 
         return self._process_response(r, action_result)
 
     def _handle_test_connectivity(self, param):
-
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
 
         self.save_progress("Querying a domain to test connectivity")
 
         # make rest call
-        ret_val, response = self._make_rest_call('myip.ms', action_result)
-        status = response.get('status') if response else None
+        ret_val, response = self._make_rest_call("myip.ms", action_result)
+        status = response.get("status") if response else None
 
-        if (phantom.is_fail(ret_val) or (status and status.lower().strip() != 'ok')):
+        if phantom.is_fail(ret_val) or (status and status.lower().strip() != "ok"):
             self.save_progress("Test Connectivity Failed")
             return action_result.get_status()
 
@@ -198,67 +191,64 @@ class MyipConnector(BaseConnector):
         return action_result.set_status(phantom.APP_SUCCESS)
 
     def _handle_whois_domain(self, param):
-
         # Implement the handler here
         # use self.save_progress(...) to send progress messages back to the platform
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress(f"In action handler for: {self.get_action_identifier()}")
         self.save_progress("Querying for domain")
 
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
-        summary = action_result.update_summary({'found': False})
+        summary = action_result.update_summary({"found": False})
 
-        domain = param['domain']
+        domain = param["domain"]
 
-        if (phantom.is_url(domain)):
+        if phantom.is_url(domain):
             domain = phantom.get_host_from_url(domain)
 
         # make rest call
         ret_val, response = self._make_rest_call(domain, action_result)
 
-        if (phantom.is_fail(ret_val)):
+        if phantom.is_fail(ret_val):
             return action_result.get_status()
 
         # Add the response into the data section
         action_result.add_data(response)
 
-        status = response.get('status')
+        status = response.get("status")
 
-        if (status) and (status.lower().strip() == 'ok'):
-            summary.update({'found': True})
+        if (status) and (status.lower().strip() == "ok"):
+            summary.update({"found": True})
 
         return action_result.set_status(phantom.APP_SUCCESS)
 
     def _handle_whois_ip(self, param):
-
         # Add an action result object to self (BaseConnector) to represent the action for this param
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress(f"In action handler for: {self.get_action_identifier()}")
         self.save_progress("Querying for IP")
 
         action_result = self.add_action_result(ActionResult(dict(param)))
-        summary = action_result.update_summary({'found': False})
+        summary = action_result.update_summary({"found": False})
 
-        ip = param['ip']
+        ip = param["ip"]
 
         # Validation for checking valid IP or not (IPV4 as well as IPV6)
         if not self._is_ip(ip):
-            return action_result.set_status(phantom.APP_ERROR, 'Please provide a valid IPV4 or IPV6 address')
+            return action_result.set_status(phantom.APP_ERROR, "Please provide a valid IPV4 or IPV6 address")
 
         # make rest call
         ret_val, response = self._make_rest_call(ip, action_result)
 
-        if (phantom.is_fail(ret_val)):
+        if phantom.is_fail(ret_val):
             return action_result.get_status()
 
         # Add the response into the data section
         action_result.add_data(response)
 
-        summary.update({'found': True})
+        summary.update({"found": True})
 
         return action_result.set_status(phantom.APP_SUCCESS)
 
     def handle_action(self, param):
-
         ret_val = phantom.APP_SUCCESS
 
         # Get the action that we are supposed to execute for this App Run
@@ -266,19 +256,18 @@ class MyipConnector(BaseConnector):
 
         self.debug_print("action_id", self.get_action_identifier())
 
-        if action_id == 'test_connectivity':
+        if action_id == "test_connectivity":
             ret_val = self._handle_test_connectivity(param)
 
-        elif action_id == 'whois_domain':
+        elif action_id == "whois_domain":
             ret_val = self._handle_whois_domain(param)
 
-        elif action_id == 'whois_ip':
+        elif action_id == "whois_ip":
             ret_val = self._handle_whois_ip(param)
 
         return ret_val
 
     def initialize(self):
-
         # Load the state in initialize, use it to store data
         # that needs to be accessed across actions
         self._state = self.load_state()
@@ -286,7 +275,7 @@ class MyipConnector(BaseConnector):
         # get the asset config
         config = self.get_config()
 
-        self._base_url = config.get('base_url').rstrip('/')
+        self._base_url = config.get("base_url").rstrip("/")
 
         try:
             self._python_version = int(sys.version_info[0])
@@ -296,14 +285,12 @@ class MyipConnector(BaseConnector):
         return phantom.APP_SUCCESS
 
     def finalize(self):
-
         # Save the state, this data is saved accross actions and app upgrades
         self.save_state(self._state)
         return phantom.APP_SUCCESS
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     import argparse
 
     import pudb
@@ -312,9 +299,9 @@ if __name__ == '__main__':
 
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument('input_test_json', help='Input Test JSON file')
-    argparser.add_argument('-u', '--username', help='username', required=False)
-    argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument("input_test_json", help="Input Test JSON file")
+    argparser.add_argument("-u", "--username", help="username", required=False)
+    argparser.add_argument("-p", "--password", help="password", required=False)
 
     args = argparser.parse_args()
     session_id = None
@@ -322,30 +309,30 @@ if __name__ == '__main__':
     username = args.username
     password = args.password
 
-    if (username is not None and password is None):
-
+    if username is not None and password is None:
         # User specified a username but not a password, so ask
         import getpass
+
         password = getpass.getpass("Password: ")
 
-    if (username and password):
+    if username and password:
         try:
             print("Accessing the Login page")
             r = requests.get(BaseConnector._get_phantom_base_url() + "login", verify=False)
-            csrftoken = r.cookies['csrftoken']
+            csrftoken = r.cookies["csrftoken"]
 
             data = dict()
-            data['username'] = username
-            data['password'] = password
-            data['csrfmiddlewaretoken'] = csrftoken
+            data["username"] = username
+            data["password"] = password
+            data["csrfmiddlewaretoken"] = csrftoken
 
             headers = dict()
-            headers['Cookie'] = 'csrftoken=' + csrftoken
-            headers['Referer'] = BaseConnector._get_phantom_base_url() + "login"
+            headers["Cookie"] = "csrftoken=" + csrftoken
+            headers["Referer"] = BaseConnector._get_phantom_base_url() + "login"
 
             print("Logging into Platform to get the session id")
             r2 = requests.post(BaseConnector._get_phantom_base_url() + "login", verify=False, data=data, headers=headers)
-            session_id = r2.cookies['sessionid']
+            session_id = r2.cookies["sessionid"]
         except Exception as e:
             print("Unable to get session id from the platfrom. Error: " + str(e))
             exit(1)
@@ -358,9 +345,9 @@ if __name__ == '__main__':
         connector = MyipConnector()
         connector.print_progress_message = True
 
-        if (session_id is not None):
-            in_json['user_session_token'] = session_id
-            connector._set_csrf_info(csrftoken, headers['Referer'])
+        if session_id is not None:
+            in_json["user_session_token"] = session_id
+            connector._set_csrf_info(csrftoken, headers["Referer"])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
